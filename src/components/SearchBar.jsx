75be28a292b81635d43bf68e28e0b7c3a38a61f7
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-06 21:02:17 -0300

    feat(SearchBar): Add hooks to avoid unnecesary re-rendering

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 export const SearchBar = () => {
-  const [input, setInput] = useState('')
+  const [searchString, setSearchString] = useState('')
 
   const url = 'http://gateway.marvel.com/v1/public/characters?'
   const apiKey = '&apikey=f4e63a51401e5c498e1740d446ae8f5d'
   const limit = '&limit=10'
 
-  async function fetchData (searchString) {
-    const fetchURL = `${url}nameStartsWith=${searchString}${limit}${apiKey}`
-    const response = await fetch(fetchURL)
-    const data = response.json()
-    console.log(data)
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      if (searchString !== '') {
+        const fetchURL = `${url}nameStartsWith=${searchString}${limit}${apiKey}`
+        const response = await fetch(fetchURL)
+        const data = await response.json()
 
-  const handleChange = (searchString) => {
-    setInput(searchString)
-    fetchData(searchString)
-  }
+        // Do something with the data
+        console.log(data)
+      }
+    }
+
+    fetchData()
+  }, [searchString])
+
+  const handleChange = useCallback((searchString) => {
+    setSearchString(searchString)
+  }, [])
 
   return (
     <div className='input-container'>
@@ -26,7 +33,7 @@ export const SearchBar = () => {
       <input
         type='text'
         placeholder='Buscar'
-        value={input}
+        value={searchString}
         onChange={(e) => handleChange(e.target.value)}
       />
     </div>
