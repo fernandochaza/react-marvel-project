a19d94601329e6f4b9b06c6ba95fbfebfd1fe8f7
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-22 12:48:54 -0300

    feat: Handle displaying/deleting search history

--- src/components/FavoriteSearchesList/FavoriteItemLink.jsx
+++ src/components/FavoriteSearchesList/FavoriteItemLink.jsx
@@ -1,13 +1,47 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import { GoHistory } from 'react-icons/go'
+import { RxCross1 } from 'react-icons/rx'
+import { deleteFavorite } from '../../Utils/deleteLocalFavorite'
+import { useCallback } from 'react'
+import { useSetAtom } from 'jotai'
+import { favoriteSearches, userInput } from '../../atoms'
 
+const DeleteFavorite = styled.span`
+  margin: 0 0 0 auto;
+`
+
+const FavoriteItem = styled.span`
+  cursor: pointer;
+`
+
+const DeleteButton = styled(RxCross1)`
+  cursor: pointer;
+`
 
 export const FavoriteItemLink = ({ text }) => {
+  const setFavorites = useSetAtom(favoriteSearches)
+  const setInput = useSetAtom(userInput)
+
+  const handleDeleteFavorite = useCallback((itemToDelete) => {
+    const newFavorites = deleteFavorite(itemToDelete)
+    setFavorites(newFavorites)
+  })
+
+  const handleSearchFavorite = useCallback((itemToSearch) => {
+    setInput(itemToSearch)
+  })
+
   return (
-    <span>
-      <GoHistory /> {text}
-    </span>
+    <>
+      <FavoriteItem onClick={() => handleSearchFavorite(text)}>
+        <GoHistory /> {text}
+      </FavoriteItem>
+      <DeleteFavorite>
+        <DeleteButton onClick={() => handleDeleteFavorite(text)} />
+      </DeleteFavorite>
+    </>
   )
 }
 
