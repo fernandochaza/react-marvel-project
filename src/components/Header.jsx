0c9c20dbbbc7a3d0276ebfa15726bc1b1bbddd03
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-22 11:36:43 -0300

    docs: minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,8 @@ import { ResultsList } from './ResultsList'
 import { Logo } from './Logo'
 import { useAtom } from 'jotai'
 import { matchingResults } from '../atoms'
+import { BsSearch } from 'react-icons/bs'
+// import { AddFavoriteSearch } from './AddFavoriteSearchButton'
 
 const StyledHeader = styled.header`
   display: flex;
@@ -12,12 +14,24 @@ const StyledHeader = styled.header`
   align-items: center;
   background-color: white;
   box-shadow: 0 2px 4px rgba(197, 197, 197, 0.5);
+
+  @media screen and (max-width: 600px){
+    flex-direction: column;
+  }
 `
 
-const Separator = styled.div`
+const VerticalDivision = styled.div`
   border-left: 2px solid rgba(0, 0, 0, 0.06);
   height: 32px;
-  margin: ${(props) => (props.$mgRight ? '0 32px' : '0 20px')};
+  margin: ${(props) => (props.$mgRight ? '0 32px 0 0' : '0 auto')};
+`
+
+const SearchIcon = styled(BsSearch)`
+  margin: 0 auto;
+  width: 24px;
+  height: 100%;
+  min-width: 24px;
+  filter: opacity(15%);
 `
 
 export const Header = () => {
@@ -28,9 +42,11 @@ export const Header = () => {
         src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg'
         alt='Marvel Logo'
       />
-      <Separator />
+      <VerticalDivision />
+      <SearchIcon />
       <SearchForm />
-      <Separator $mgRight />
+      {/* <AddFavoriteSearch alt='Add to favorite button' /> */}
+      <VerticalDivision $mgRight />
       <ResultsList results={searchResults} />
     </StyledHeader>
   )
