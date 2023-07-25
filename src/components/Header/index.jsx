4b2229f6a515f438ac659f673184b076021d442e
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-25 09:28:07 -0300

    chore: Add favicon link, remove blank lines

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,18 +6,22 @@ import { Logo } from './Logo'
 import { ResultsList } from '../SearchForm/ResultsList'
 
 import { StyledHeader, VerticalDivision } from './styles'
+import { Outlet } from 'react-router-dom'
 
 export const Header = () => {
   const [searchResults] = useAtom(matchingResults)
   return (
-    <StyledHeader>
-      <Logo
-        src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg'
-        alt='Marvel Logo'
-      />
-      <SearchForm />
-      <ResultsList results={searchResults} />
-      <VerticalDivision />
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <Logo
+          src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg'
+          alt='Marvel Logo'
+        />
+        <SearchForm />
+        <ResultsList results={searchResults} />
+        <VerticalDivision />
+      </StyledHeader>
+      <Outlet />
+    </>
   )
 }
