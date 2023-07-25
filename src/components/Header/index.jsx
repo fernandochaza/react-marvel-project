81fe5dc60b5567970ff52622e0d5d613dbae83c8
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-25 17:14:10 -0300

    feat: Hide search bar in comic view

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,10 +6,19 @@ import { Logo } from './Logo'
 import { ResultsList } from '../SearchForm/ResultsList'
 
 import { StyledHeader, VerticalDivision } from './styles'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export const Header = () => {
   const [searchResults] = useAtom(matchingResults)
+  const [isComicView, setIsComicView] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    const isComicView = location.pathname.includes('/comic')
+    setIsComicView(isComicView)
+  }, [location.pathname])
+
   return (
     <>
       <StyledHeader>
@@ -17,7 +26,7 @@ export const Header = () => {
           src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg'
           alt='Marvel Logo'
         />
-        <SearchForm />
+        {!isComicView ? <SearchForm /> : ''}
         <ResultsList results={searchResults} />
         <VerticalDivision />
       </StyledHeader>
