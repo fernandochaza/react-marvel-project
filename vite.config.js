da226991accdf95fee4e734cbf0cc670f06f51ce
Fernando <111901603+fernandochaza@users.noreply.github.com>
2023-07-27 21:48:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                fetch: resolve(__dirname, 'fetch.html'),
-                key: resolve(__dirname, 'filename.html'),
-
-            }
-        }
-    },
-    base: '/marvel-searcher/',
-    plugins: [react()]
-});
+  base: '/marvel-searcher/',
+  plugins: [react()]
+})
