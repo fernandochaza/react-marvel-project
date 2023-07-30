31852a7af95f5c5cbe1ae513d3c03cda1f917de1
Fernando Chazarreta <fernandochaza@gmail.com>
2023-07-30 16:56:45 -0300

    feat: Fix header and add top margin to views

--- src/components/CardsView/CardsPagination/styles.js
+++ src/components/CardsView/CardsPagination/styles.js
@@ -8,12 +8,14 @@ const CardsContainer = styled.div`
   place-items: center;
   max-width: 1180px;
   margin: 0 auto;
+  margin-top: 72px;
   padding: 2rem 80px;
   background-color: ${(props) => props.theme.mainBg};
   transition: background-color .75s ease;
 
   @media screen and (max-width: ${(props) => props.theme.breakpointSm}) {
     padding: 2rem 20px;
+    margin-top: 116px;
   }
 `
 
